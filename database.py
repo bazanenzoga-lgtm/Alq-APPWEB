426c226c21e7ffd51437d5eb621220f798ebe994
Enzo Bazan <enzogbazan256@gmail.com>
2025-12-26 16:18:21 -0300

    Gbasedatos(PGS)

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./alquileres.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Rogger3865@127.0.0.1:5432/Alq-APP"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
