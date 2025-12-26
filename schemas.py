83fe3ab78a1b498c17524bbcccf94387a3aa3daa
Enzo Bazan <enzogbazan256@gmail.com>
2025-12-26 16:19:29 -0300

    nClss

--- schemas.py
+++ schemas.py
@@ -1,7 +1,27 @@
 from pydantic import BaseModel, EmailStr
 
+class UserBase(BaseModel):
+    email: EmailStr
+    is_owner: bool
+
+
 
 class UserCreate(BaseModel):
-    email: EmailStr
+    email: str
+    full_name: str  
     password: str
     is_owner: bool
+
+class UserUpdate(BaseModel):
+    full_name: str | None = None 
+    email: EmailStr | None = None 
+    password: str | None = None
+    is_owner: bool | None = None
+
+class UserOut(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
