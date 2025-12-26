de16254b334cb3e28397480e37cf3b93d453c83f
Enzo Bazan <enzogbazan256@gmail.com>
2025-12-26 16:18:43 -0300

    nVbls

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
-
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    id = Column(Integer, primary_key=True, index=True)  # PK
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, nullable=True)
+    password = Column(String, nullable=False)
     is_owner = Column(Boolean, default=False)
